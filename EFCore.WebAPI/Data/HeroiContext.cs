8a12de4b32b38230c521345053525e58819f3c1b
Gustavo G <Gustavo G@DESKTOP-JTE51H5>
2023-12-05 16:56:00 -0300

    Add project files.

---
+++ EFCore.WebAPI/Data/HeroiContext.cs
@@ -0,0 +1,28 @@
+﻿using EFCore.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.WebAPI.Data
+{
+    public class HeroiContext : DbContext
+    {
+        public DbSet<Heroi> Herois { get; set; }
+        public DbSet<Batalha> Batalhas { get; set; }
+        public DbSet<Arma> Armas { get; set; }
+        public DbSet<HeroiBatalha> HeroisBatalhas { get; set; }
+        public DbSet<IdentidadeSecreta> IdentidadesSecretas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Password=rockwins11;Persist Security Info=True;User ID=sa;Initial Catalog=HeroApp;Data Source=DESKTOP-JTE51H5;TrustServerCertificate=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HeroiBatalha>(entity =>
+            {
+                entity.HasKey(e => new {e.BatalhaId, e.HeroiId});
+            });
+        }
+
+    }
+}
